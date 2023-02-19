d8936f47f5d800dd999a1c745fdfa34a784bace2
Rachel Brown Gomez <rgbrown21@icloud.com>
2023-02-19 16:23:49 -0500

    update json files, change http to https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=f3e04adc'
+const API_URL = 'https://www.omdbapi.com?apikey=f3e04adc'
 
 const App = () => {
     const [movies, setMovies] = useState([]);
